d9f5a4d18b3c4c18ce918a4a8f4d8e62e85ca9bf
flaviorv <frezendevaroto@gmail.com>
2024-12-19 00:46:53 -0300

    dinamicly generation of rabbitmq queues, exchange and routing-keys///front was added to docker-compose file

---
+++ musichouse_products/src/main/java/com/musichouse/model/rabbitmq/MsgConfig.java
@@ -0,0 +1,56 @@
+package com.musichouse.model.rabbitmq;
+
+import org.springframework.amqp.core.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MsgConfig {
+
+    @Bean public Queue queue()
+    {
+        return new Queue("sale_queue", false);
+    }
+
+    @Bean public Queue queue2()
+    {
+        return new Queue("product_queue", false);
+    }
+
+    @Bean public Queue queue3()
+    {
+        return new Queue("payment_queue", false);
+    }
+
+    @Bean public Exchange exchange()
+    {
+        return new DirectExchange("sales_exc");
+    }
+
+    @Bean
+    public Binding bindingSaleQueue()
+    {
+        return BindingBuilder.bind(queue())
+                .to(exchange())
+                .with("payment_status_rk")
+                .noargs();
+    }
+
+    @Bean
+    public Binding bindingProductQueue()
+    {
+        return BindingBuilder.bind(queue2())
+                .to(exchange())
+                .with("approved_payment_rk")
+                .noargs();
+    }
+
+    @Bean
+    public Binding bindingPaymentQueue()
+    {
+        return BindingBuilder.bind(queue3())
+                .to(exchange())
+                .with("check_payment_rk")
+                .noargs();
+    }
+}
