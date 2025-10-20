c92db80093da8acbe1dbbce85f7b2aea67c2ff49
flaviorv <frezendevaroto@gmail.com>
2025-10-20 01:38:01 -0300

    Search field exceptions handling.

--- mh-front/src/pages/Products.jsx
+++ mh-front/src/pages/Products.jsx
@@ -1,59 +1,70 @@
 import axios from "axios";
 import Table from "react-bootstrap/Table";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import "./Products.css";
 
 function Products() {
   const [products, setProducts] = useState([]);
+  const [status, setStatus] = useState(null);
+  const [msg, setMsg] = useState(null);
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
 
-  const getProducts = async () => {
+  const getProducts = useCallback(async () => {
     const body = {};
-    if (searchParams.get("q")) {
-      body.q = searchParams.get("q");
-    }
+    body.q = searchParams.get("q");
     try {
-      console.log(body);
       const response = await axios.post("http://localhost:9999/product/search", body);
       const data = response.data;
+
+      setStatus(200);
+      setMsg(null);
       setProducts(data);
-      console.log("Products were loaded successfuly: ", data);
     } catch (error) {
-      console.log(error);
-      console.log("No products");
-      return [];
+      const statusCode = error.response.status;
+      setStatus(statusCode);
+      if (statusCode === 404) {
+        setMsg("No products found.");
+      } else if (statusCode === 400) {
+        setMsg("Input should not be empty or contain only special characters.");
+      } else {
+        setMsg("Sorry. A problem ocurred.");
+      }
     }
-  };
+  }, [searchParams]);
 
   useEffect(() => {
     getProducts();
-  }, [searchParams]);
+  }, [getProducts]);
 
   return (
     <div id="products">
       <h1 className="title">Products</h1>
-      <Table className="products-table">
-        <thead className="table-head">
-          <tr>
-            <th>Brand</th>
-            <th>Model</th>
-            <th>Stock</th>
-            <th>Price</th>
-          </tr>
-        </thead>
-        <tbody>
-          {products.map((product) => (
-            <tr className="product-row" key={product.model} onClick={() => navigate("/detailed", { state: { productId: product.model } })}>
-              <td>{product.brand}</td>
-              <td>{product.model}</td>
-              <td>{product.quantity} units</td>
-              <td className="price">$ {product.price.toFixed(2)}</td>
+      {status !== 200 ? (
+        <b>{msg}</b>
+      ) : (
+        <Table className="products-table">
+          <thead className="table-head">
+            <tr>
+              <th>Brand</th>
+              <th>Model</th>
+              <th>Stock</th>
+              <th>Price</th>
             </tr>
-          ))}
-        </tbody>
-      </Table>
+          </thead>
+          <tbody>
+            {products.map((product) => (
+              <tr className="product-row" key={product.model} onClick={() => navigate("/detailed", { state: { productId: product.model } })}>
+                <td>{product.brand}</td>
+                <td>{product.model}</td>
+                <td>{product.quantity} units</td>
+                <td className="price">$ {product.price.toFixed(2)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
     </div>
   );
 }
