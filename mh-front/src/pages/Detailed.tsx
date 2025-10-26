0cf9c5c89e4d03fefbd5d698a417d9f23abf0d0c
flaviorv <frezendevaroto@gmail.com>
2025-10-25 21:10:44 -0300

    Image zomm in the Detailed page.

--- mh-front/src/pages/Detailed.tsx
+++ mh-front/src/pages/Detailed.tsx
@@ -4,6 +4,8 @@ import { IProductProps, Product } from "../types/Product.ts";
 import { capitalize } from "../utils/formatter.ts";
 import { AddCart } from "../components/AddCart.tsx";
 import { SpecificationsTable } from "../components/SpecificationsTable.tsx";
+import Zoom from "react-medium-image-zoom";
+import "react-medium-image-zoom/dist/styles.css";
 
 function Detailed() {
   const state = useLocation().state as IProductProps | null;
@@ -16,7 +18,14 @@ function Detailed() {
   return (
     <div id="detailed">
       <section id="product-info">
-        <img id="img" src={`data:image/png;base64,${product.image}`} alt={`${product.type}`} />
+        <Zoom
+          zoomImg={{
+            src: `data:image/png;base64,${product.image}`,
+            alt: `${product.type} zoom`,
+          }}
+        >
+          <img id="detailed-img" src={`data:image/png;base64,${product.image}`} alt={`${product.type}`} />
+        </Zoom>
         <div id="add-cart-section">
           <h2 className="add-cart-title">
             {capitalize(product.type)} {product.brand} {product.model}
